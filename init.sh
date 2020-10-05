f2862fabd361aaf58586240d8c10a39ee38f1e43
Vladimirs Semikins <vladimirs.semikins@gmail.com>
2020-10-05 12:34:42 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -112,7 +112,12 @@ install_dotfiles () {
   done
 }
 
+install_nvm(){
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
+}
+
 install_dotfiles
+install_nvm
 
 echo ''
 echo '  All installed!'
